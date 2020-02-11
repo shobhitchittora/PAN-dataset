769e2410fea416795b0cbb287e19da8da8d12ef0
Shobhit Chittora <chittorashobhit@gmail.com>
2020-02-11 11:55:10 +0530

    removes extra files

--- text.js
+++
@@ -1,8 +0,0 @@
-const gm = require('gm');
-
-gm(125, 30, "#accadb ")
-  .font("Frygia.ttf", 18)
-  .drawText(5, 20, "AAABU9876X", 100)
-  .write("number.jpg", function (err) {
-    console.log(err);
-  });
